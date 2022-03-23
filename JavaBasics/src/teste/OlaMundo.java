c2c49eef2e5a81aba8b8648816100dca928a4862
bragaLeandro <rm94513@fiap.com.br>
2022-03-23 20:57:53 -0300

    Java basic

---
+++ JavaBasics/src/teste/OlaMundo.java
@@ -0,0 +1,17 @@
+package teste;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo!!!");
+		//declaração de variável
+		String nome = "Leandro";
+		byte idade = 19;
+		float peso = 60.8f;
+		
+		//concatenação
+		System.out.println("Olá, " + nome + " você tem " + idade + " anos.");
+
+	}
+
+}
