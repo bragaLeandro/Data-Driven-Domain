fbea5eca6ac7600119ac3c3ce34439513202746f
bragaLeandro <rm94513@fiap.com.br>
2022-06-14 13:20:24 -0300

    hello world

---
+++ helloWorld/src/teste/Teste.java
@@ -0,0 +1,12 @@
+package teste;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// Meu milésimo Hello world
+		System.out.println("Hello world");
+		System.out.println("Hello world 2");
+		System.out.println("Hello world 3");
+	}
+
+}
